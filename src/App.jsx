96e7262abebf277e6c71986d7d75a2219118a86f
unknown <dev@NSN1191261234.edu119.local>
2026-02-13 11:05:02 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+export function App() {
+  return (
+    <>
+      <h1>Bem-vindo</h1>
+      <p>Digite suas credenciais para acessar o sistema.</p>
+      <input type="Email" placeholder="Digite seu Email" />
+      <input type="Password" placeholder="Digite sua Senha" />
+      <button>Entrar</button>
+    </>
+  );
+}
